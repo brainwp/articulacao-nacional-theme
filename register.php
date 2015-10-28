3157708246e504db83c022404285d77237385d54
Diego Rojas <diego@brasa.art.br>
2015-10-28 16:28:03 -0200

    register.php limpo e concerto no css da sidebar

--- register.php
+++ register.php
@@ -13,6 +13,9 @@ get_header();
 			<h2 class="titulo">
 				Já possui conta? Então <a href="<?php echo wp_login_url();?>">faça login</a>!
 			</h2><!-- .hentry -->
+			<p>
+				Redefinir senha? <a href="<?php echo wp_lostpassword_url();?>">Clique aqui</a>!
+			</p><!-- .hentry -->
 			<li class="hentry">
 				<form action="<?php echo home_url();?>?do_register=true" id="form-register" method="POST">
 					<label>
@@ -20,41 +23,8 @@ get_header();
 						<input type="text" name="name" required>
 					</label>
 					<label>
-						Idade:
-						<input type="number" name="idade" min="1" required>
-					</label>
-					<label>
-						Polo:
-						<?php if( !is_multisite() ) : ?>
-							<input type="text" name="polo" required>
-						<?php else : ?>
-							<?php $current = get_current_blog_id();?>
-							<?php $sites = wp_get_sites();?>
-							<select name="polo" required>
-								<?php foreach ($sites as $site) : ?>
-									<?php if( $site['blog_id'] == $current ) continue; ?>
-									<?php $infos = get_blog_details( array( 'blog_id' => $site['blog_id'] ) );?>
-									<option value="<?php echo esc_attr( $infos->blogname );?>"><?php echo $infos->blogname;?></option>
-								<?php endforeach;?>
-							</select>
-						<?php endif;?>
-					</label>
-					<label>
 						Telefone:
 						<input type="text" name="fone" required pattern="\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}">
-					</label>
-					<label>
-						No projeto você é:
-						<select name="user_type" required>
-							<option value="Mediador(a)">Mediador(a)</option>
-							<option value="Jogador(a)">Jogador(a)</option>
-							<option value="Educador(a)">Educador(a)</option>
-							<option value="Outros">Outros</option>
-						</select>
-					</label>
-					<label style="display:none;" id="label-user-type">
-						Especifique sua função
-						<input type="text" name="user_type_txt">
 					</label>
 					<label>
 						E-mail / Nome de usuário:
